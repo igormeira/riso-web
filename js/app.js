705bc5b7bddaac6c3648faa6db753732017217b8
Igor Meira <igor.meira@gmail.com>
2017-10-30 10:56:02 -0300

    Adicionando modal para ajuda

--- js/app.js
+++ js/app.js
@@ -9,30 +9,4 @@
  * Main module of the application.
  */
 var app;
-app = angular.module('risoApp', ['ngRoute', 'directivesNavFoot']);
-
-    app.config(function($routeProvider) {
-
-        $routeProvider.when('/',  {
-            temaplateUrl: "views/main.html",
-            controller: 'MainCtrl'
-        });
-
-        $routeProvider.when('/enrich',  {
-            temaplateUrl: "views/enrich.html",
-            controller: 'MainCtrl'
-        });
-
-        $routeProvider.when('/about',  {
-            temaplateUrl: "views/about.html"
-        });
-
-        $routeProvider.when('/help',  {
-            temaplateUrl: "views/help.html"
-        });
-
-        $routeProvider.otherwise({
-            redirectTo: '/'
-        });
-
-    });
+app = angular.module('risoApp', ['directivesNavFoot']);
